0d8af6a86e03db4dfb6b6215ac196210dce77f29
mak <markraul.delgado@gmail.com>
2024-06-29 12:40:49 +0800

    installed date-picker for vue

--- resources/js/components/product/Add.vue
+++ resources/js/components/product/Add.vue
@@ -9,7 +9,7 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form @submit.prevent="addProduct">
+                    <form @submit.prevent="addProduct" ref="addProductForm">
                         <div class="form-group">
                             <label for="name">Name</label>
                             <input type="text" class="form-control" id="name" v-model="newProduct.name" required>
@@ -21,6 +21,14 @@
                         <div class="form-group">
                             <label for="description">Description</label>
                             <textarea class="form-control" id="description" v-model="newProduct.description" required></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="datetime">Date and Time</label>
+                            <datetime-picker v-model="newProduct.datetime" class="form-control" id="datetime"></datetime-picker>
+                        </div><br>
+                        <div class="form-group">
+                            <label for="images">Images</label>
+                            <input type="file" id="images" ref="images" multiple accept="image/*">
                         </div>
                         <br>
                         <button type="submit" class="btn btn-primary">Add Product</button>
@@ -36,6 +44,7 @@ import { ref } from 'vue';
 import axios from 'axios';
 import { API_ADD_PRODUCT } from '../../config.js';
 import { defineProps, defineEmits } from 'vue';
+import DatetimePicker from 'vue3-datepicker';
 
 const props = defineProps({
     showAddModal: {
@@ -49,7 +58,8 @@ const emit = defineEmits(['close']);
 const newProduct = ref({
     name: '',
     category: '',
-    description: ''
+    description: '',
+    datetime: null, // Initialize datetime field
 });
 
 const closeAddModal = () => {
@@ -58,32 +68,45 @@ const closeAddModal = () => {
     newProduct.value = {
         name: '',
         category: '',
-        description: ''
+        description: '',
+        datetime: null,
     };
 };
 
 const addProduct = async () => {
-    
     try {
+        // Validate form before proceeding
+        if (!$refs.addProductForm.validate()) {
+            return;
+        }
 
-        console.log(newProduct.value);
-        const response = await axios.post(API_ADD_PRODUCT, newProduct.value);
-        console.log(response);
+        const formData = new FormData();
+        formData.append('name', newProduct.value.name);
+        formData.append('category', newProduct.value.category);
+        formData.append('description', newProduct.value.description);
+        formData.append('datetime', newProduct.value.datetime); // Include datetime field
+        
+        // Append images to form data
+        const images = $refs.images.files;
+        for (let i = 0; i < images.length; i++) {
+            formData.append('images[]', images[i]);
+        }
+
+        const config = {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        };
+
+        const response = await axios.post(API_ADD_PRODUCT, formData, config);
         closeAddModal();
-
         const addedProduct = response.data;
         window.dispatchEvent(new CustomEvent('product-added', { detail: addedProduct }));
-
         
     } catch (error) {
-        if (error.response && error.response.status === 422) {
-            errors.value = error.response.data.errors;
-        } else {
-            console.error('Error adding product:', error);
-        }
+        console.error('Error adding product:', error);
     }
 };
-
 </script>
 
 <style scoped>
